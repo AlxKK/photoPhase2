a139f7d22e8b2e70b2ca122ee13392e25548b19d
AlxKK <bobbytop@bk.ru>
2022-04-27 15:06:58 +0300

    release 0

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const config = require('./config/config');
+const mainRouter = require('./routes/main.routes');
+
+const app = express();
+config(app);
+
+// app.use('/', mainRout);
+app.use('/', mainRouter);
+
+
+
+app.listen(3000, () => console.log("listen port 3000"));
+// app.use('/', mainRouter)
+// app.use('/', mainRout);
